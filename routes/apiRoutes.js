610ca26ebc99020ab3384a24ae311fa37655173a
jmbw1988 <jmbw1988@gmail.com>
2019-02-25 20:21:40 -0500

    routes work!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -68,134 +68,92 @@ module.exports = function(app) {
   });
       });
     });
-  };
+  
   
   
 // // POST route to store user comments in the database.
 
-// app.post("/api/comment", function(req, res) {
-//   console.log(req.body);
-//   db.Input.create({
-//     username: req.body.username,
-//     city : req.body.city,
-//     country: req.body.country,
-//     comment: req.body.comment,
-//   })
-//     .then(function(dbInput) {
-//       console.log(dbInput)
-//       res.json(dbInput);
-//     });
-// });
+app.post("/api/comment", function(req, res) {
+  console.log(req.body);
+  db.Input.create({
+    username: req.body.username,
+    city : req.body.city,
+    country: req.body.country,
+    comment: req.body.comment,
+  })
+    .then(function(dbInput) {
+      console.log(dbInput)
+      res.json(dbInput);
+    });
+});
 
 
-// // Store user suggestions in the database.
+// Store user suggestions in the database.
 
-// app.post("/api/add", function(req, res) {
-//   console.log(req.body);
-//   db.Input.create({
-//     username: req.body.username,
-//     city : req.body.city,
-//     country: req.body.country,
-//     description: req.body.description,
-//   })
-//     .then(function(dbInput) {
-//       res.json(dbInput);
-//     });
-// });
+app.post("/api/add", function(req, res) {
+  console.log(req.body);
+  db.Input.create({
+    username: req.body.username,
+    city : req.body.city,
+    country: req.body.country,
+    description: req.body.description,
+  })
+    .then(function(dbInput) {
+      console.log(dbInput);
+      res.json(dbInput);
+    });
+});
 
 
 // // Search for Footprint in location table by city.
 
-// app.get("/api/search/city/:city", function(req, res) {
-//   db.Locations.findAll({
-//     where: {
-//       city: req.params.city
-//     }
-//   }).then(function(dbLocations) {
-//     res.json(dbLocations);
-//   });
-// });
+app.get("/api/search/city", function(req, res) {
+  db.Locations.findAll({
+    where: {
+      city: req.body.city
+    }
+  }).then(function(dbLocations) {
+    res.json(dbLocations);
+  });
+});
 
 // // Generate 3 location cards for the search home page. 
 
-// app.get("/api/search", function(req, res) {
-//   db.Locations.findAll({
-//     ORDER: id DESC,
-//     limit: 3
-//   }).then(function(dbLocations) {
-//     res.json(dbLocations);
-//   });
-// });
+app.get("/api/search", function(req, res) {
+  db.Locations.findAll({
+    order: [['id' ,'DESC']],
+    limit: 3,
+  }).then(function(dbLocations) {
+    console.log(dbLocations);
+    res.json(dbLocations);
+  });
+});
 
 // // Deleteing a comment.
-// app.delete("/api/comment/:id", function(req, res) {
-//   db.Input.destroy({
-//     where: {
-//       id: req.params.id
-//     }
-//   })
-//     .then(function(dbInput) {
-//       res.json(dbInput);
-//     });
-// });
+app.delete("/api/comment/:id", function(req, res) {
+  db.Input.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(function(dbInput) {
+      res.json(dbInput);
+    });
+});
 
-// app.put("/api/comment", function(req, res) {
-//   db.Input.update(req.body.comment,
-//     {
-//       where: {
-//         id: req.body.id
-//       }
-//     })
-//     .then(function(dbInput) {
-//       res.json(dbInput);
-//     });
-// });
+app.put("/api/comment", function(req, res) {
+  db.Input.update(req.body.comment,
+    {
+      where: {
+        id: req.body.id
+      }
+    })
+    .then(function(dbInput) {
+      res.json(dbInput);
+    });
+});
 
-// }
+};
 
 
-
-// function compare(users) {  
-
-//   // Empty variables to store results
-//     let bestMatch = [];
-//     let smallestDifference;
-//     const differences = [];
-  
-//   // Takes each location score and converts it into a number, then pushes it into one array. 
-//     db.Locations.findAll({}).then(function(dbLocations) {
-//     for (let i=0; i < dbLocations.length; i++) {
-  
-//   // Take the difference of each score and push it into it's own array.
-//     differences.push(Math.abs((users.nature) - (dblocations.nature)));
-//     differences.push(Math.abs((users.cost) - (dblocations.cost)));
-//     differences.push(Math.abs((users.history) - (dblocations.history)));
-//     differences.push(Math.abs((users.adventure) - (dblocations.adventure)));
-//     differences.push(Math.abs((users.urban) - (dblocations.urban)));
-//     differences.push(Math.abs((users.activity) - (dblocations.activity)));
-//     differences.push(Math.abs((users.culture) - (dblocations.cuture)));
-//     differences.push(Math.abs((users.climate) - (dblocations.climate)));
-//     differences.push(Math.abs((users.access) - (dblocations.access)));
-//     differences.push(Math.abs((users.stay) - (dblocations.stay)));
-  
-//   // Get the sum of the differences array.
-//     const result = differences.recude((acc, curr) => {
-//       return acc + curr;
-//       }, 0);
-  
-//   // If no Best Match exists, set current location as the best match and the smallestDifference as the current number.
-//       if(!bestMatch) {
-//         bestMatch = locations;
-//         smallestDifference = result;
-//       }
-  
-//   // If a best match exists, compare the current result to the smallest difference, and replace if less.
-//       else if(result < smallestDifference) {
-//         bestMatch = location;
-//         smallestDifference = results;
-//       }
-//       console.log(bestMatch)
-//   };
-//   });
-// };
 
